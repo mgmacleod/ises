7ece62bab01900ea2b7c47136bcd5f7887ad2dfd
Matthew MacLeod <mgmacleod8@gmail.com>
2023-05-15 20:56:06 -0400

    GH-4: added spring boot dependencies and deleted old `Main` class

---
+++ ises/src/main/java/ises/IsesApplication.java
@@ -0,0 +1,13 @@
+package ises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsesApplication.class, args);
+	}
+
+}
