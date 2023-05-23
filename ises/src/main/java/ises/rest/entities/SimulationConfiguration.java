7b157f9037676b8d422e334e86a7a8e7ed93441c
Matthew MacLeod <mgmacleod8@gmail.com>
2023-05-23 09:17:18 -0400

    GH-14: added validation annotations and some basic unit tests

--- ises/src/main/java/ises/rest/entities/SimulationConfiguration.java
+++ ises/src/main/java/ises/rest/entities/SimulationConfiguration.java
@@ -11,6 +11,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 /**
  * Represents all of the configuration values needed to control a simulation run.
@@ -30,108 +32,172 @@ public class SimulationConfiguration {
 	////////////////////////// Model parameters \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 	// shape parameters
+	@NotNull
+	@Positive
 	@Column(name = "shape_max", nullable = false)
 	private Integer shapeMax = 128; // the size of the shape space
 
+	@NotNull
+	@Positive
 	@Column(name = "distance_max", nullable = false)
 	private Integer distanceMax = 3; // the maximum 'distance' in shapes for binding
 
 	// translation costs
+	@NotNull
+	@Positive
 	@Column(name = "cost_rna", nullable = false)
 	private Integer costRNA = 3; // per translation event
 
+	@NotNull
+	@Positive
 	@Column(name = "cost_protein", nullable = false)
 	private Integer costProtein = 2; // per translated protein
 
 	// basal translation rate
+	@NotNull
+	@Positive
 	@Column(name = "basal_translation_rate", nullable = false)
 	private Double basalTranslationRate = 0.01;
 
 	// max protein production and degradation rates added.
+	@NotNull
+	@Positive
 	@Column(name = "max_degradation_rate", nullable = false)
 	private Integer maxDegradationRate = 10; // max timesteps a protein can survive
 
+	@NotNull
+	@Positive
 	@Column(name = "max_production_rate", nullable = false)
 	private Integer maxProductionRate = 10; // max number of proteins a gene can produce
 
 	// energy signalling thresholds
+	@NotNull
+	@Positive
 	@Column(name = "energy_1_threshold", nullable = false)
 	private Integer energy1Threshold = 500;
 
+	@NotNull
+	@Positive
 	@Column(name = "energy_2_threshold", nullable = false)
 	private Integer energy2Threshold = 333;
 
 	// energy yields from food (low, med, high)
 	// the amount of energy produced when each type of food is available
+	@NotNull
+	@Positive
 	@Column(name = "energy_1_production", nullable = false)
 	private Integer energy1Production = 5;
 
+	@NotNull
+	@Positive
 	@Column(name = "energy_2_production", nullable = false)
 	private Integer energy2Production = 5;
 
+	@NotNull
+	@Positive
 	@Column(name = "energy_3_production", nullable = false)
 	private Integer energy3Production = 10;
 
+	@NotNull
+	@Positive
 	@Column(name = "energy_4_production", nullable = false)
 	private Integer energy4Production = 10;
 
+	@NotNull
+	@Positive
 	@Column(name = "energy_5_production", nullable = false)
 	private Integer energy5Production = 15;
 
+	@NotNull
+	@Positive
 	@Column(name = "energy_6_production", nullable = false)
 	private Integer energy6Production = 15;
 
+	@NotNull
+	@Positive
 	@Column(name = "energy_7_production", nullable = false)
 	private Integer energy7Production = 20;
 
+	@NotNull
+	@Positive
 	@Column(name = "energy_8_production", nullable = false)
 	private Integer energy8Production = 20;
 
+	@NotNull
+	@Positive
 	@Column(name = "energy_9_production", nullable = false)
 	private Integer energy9Production = 25;
 
 	// biomass yields from biosynthesis pathways (SAO)
+	@NotNull
+	@Positive
 	@Column(name = "biomass_1_production", nullable = false)
 	private Integer biomass1Production = 50;
 
+	@NotNull
+	@Positive
 	@Column(name = "biomass_2_production", nullable = false)
 	private Integer biomass2Production = 10;
 
+	@NotNull
+	@Positive
 	@Column(name = "biomass_3_production", nullable = false)
 	private Integer biomass3Production = 50;
 
+	@NotNull
+	@Positive
 	@Column(name = "biomass_4_production", nullable = false)
 	private Integer biomass4Production = 10;
 
 	// costs for biosynthesis pathways (SAO)
+	@NotNull
+	@Positive
 	@Column(name = "biomass_1_cost", nullable = false)
 	private Integer biomass1Cost = 75;
 
+	@NotNull
+	@Positive
 	@Column(name = "biomass_2_cost", nullable = false)
 	private Integer biomass2Cost = 75;
 
+	@NotNull
+	@Positive
 	@Column(name = "biomass_3_cost", nullable = false)
 	private Integer biomass3Cost = 5;
 
+	@NotNull
+	@Positive
 	@Column(name = "biomass_4_cost", nullable = false)
 	private Integer biomass4Cost = 5;
 
 	// stress parameters
+	@NotNull
+	@Positive
 	@Column(name = "stress_1_production", nullable = false)
 	private Integer stress1Production = 25; // number of stress molecules removed per rsp activation
 
+	@NotNull
+	@Positive
 	@Column(name = "stress_2_production", nullable = false)
 	private Integer stress2Production = 25;
 
+	@NotNull
+	@Positive
 	@Column(name = "stress_1_cost", nullable = false)
 	private Integer stress1Cost = 100; // cost per activation
 
+	@NotNull
+	@Positive
 	@Column(name = "stress_2_cost", nullable = false)
 	private Integer stress2Cost = 100;
 
+	@NotNull
+	@Positive
 	@Column(name = "stress_1_threshold", nullable = false)
 	private Integer stress1Threshold = 100; // critical threshold; model dies if reached or exceeded
 
+	@NotNull
+	@Positive
 	@Column(name = "stress_2_threshold", nullable = false)
 	private Integer stress2Threshold = 100;
 
@@ -139,9 +205,13 @@ public class SimulationConfiguration {
 	/////////////////////////// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 	// max time and starting energy (as in original)
+	@NotNull
+	@Positive
 	@Column(name = "start_energy", nullable = false)
 	private Integer startEnergy = 1000; // the amount of energy with which each model starts
 
+	@NotNull
+	@Positive
 	@Column(name = "max_time", nullable = false)
 	private Integer maxTime = 2000; // number of timesteps in the simulation
 
@@ -191,73 +261,112 @@ public class SimulationConfiguration {
 	@Transient
 	private Double food9Rate = 0.0;
 
+	@NotNull
+	@Positive
 	@Column(name = "food_rate_base", nullable = false)
 	private Double foodRateBase = 0.08;
 
+	@NotNull
+	@Positive
 	@Column(name = "food_rate_factor", nullable = false)
 	private Double foodRateFactor = 2.0;
 
+	@NotNull
+	@Positive
 	@Column(name = "foog_flip_interval", nullable = false)
 	private Integer foodFlipInterval = 100;
 
-	// simulation-side stress parameters (as in original)
+	// simulation-side stress parameters
+	@NotNull
+	@Positive
 	@Column(name = "stress_in_production", nullable = false)
 	private Integer stressInProduction = 25; // the stress increment; number of molecules added
 
+	@NotNull
+	@Positive
 	@Column(name = "stress_in_interval", nullable = false)
 	private Integer stressInInterval = 25; // number of timesteps before stress enters
 
 	////////////////////////////////// GA parameters \\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
+	@NotNull
+	@Positive
 	@Column(name = "population_size", nullable = false)
 	private Integer populationSize = 100;
 
+	@NotNull
+	@Positive
 	@Column(name = "max_generation", nullable = false)
 	private Integer maxGeneration = 10;
 
+	@NotNull
+	@Positive
 	@Column(name = "neutral_generations", nullable = false)
 	private Integer neutralGenerations = 100; // number of generations of neutral evolution
 
+	@NotNull
+	@Positive
 	@Column(name = "init_genome_size", nullable = false)
 	private Integer initGenomeSize = 32;
 
 	// mutation probabilities
+	@NotNull
+	@Positive
 	@Column(name = "gene_dup_probability", nullable = false)
 	private Double geneDupProbability = 0.001; // gene duplication
 
+	@NotNull
+	@Positive
 	@Column(name = "gene_del_probability", nullable = false)
 	private Double geneDelProbability = 0.001; // gene deletion
 
+	@NotNull
+	@Positive
 	@Column(name = "prot_shape_mut_probability", nullable = false)
 	private Double protShapeMutProbability = 0.005; // protein shape mutation
 
+	@NotNull
+	@Positive
 	@Column(name = "prot_prod_mut_probability", nullable = false)
 	private Double protProdMutProbability = 0.005; // protein production rate mutation
 
+	@NotNull
+	@Positive
 	@Column(name = "prot_deg_mut_probability", nullable = false)
 	private Double protDegMutProbability = 0.005; // protein degradation rate mutation
 
+	@NotNull
+	@Positive
 	@Column(name = "bind_site_dup_probability", nullable = false)
 	private Double bindSiteDupProbability = 0.008; // binding site duplication
 
+	@NotNull
+	@Positive
 	@Column(name = "bind_site_del_probability", nullable = false)
 	private Double bindSiteDelProbability = 0.008; // binding site deletion
 
+	@NotNull
+	@Positive
 	@Column(name = "bind_site_shape_mut_probability", nullable = false)
 	private Double bindSiteShapeMutProbability = 0.0008; // binding site shape mutation
 
+	@NotNull
+	@Positive
 	@Column(name = "bind_site_flip_probability", nullable = false)
 	private Double bindSiteFlipProbability = 0.0008; // binding site regulatory flip
 
 	// data collection intervals
-	// in use when 'collect data?' is selected in the ui
 
 	// the gene regulatory network of the best model will be saved every x
 	// generations
+	@NotNull
+	@Positive
 	@Column(name = "sample_grn_interval", nullable = false)
 	private Integer sampleGrnInterval = 100;
 
 	// the pertinent data from the best model will be saved every x generations
+	@NotNull
+	@Positive
 	@Column(name = "sample_model_interval", nullable = false)
 	private Integer sampleModelInterval = 50;
 
--- ises/src/test/java/ises/rest/entities/SimulationConfigurationTest.java
+++ ises/src/test/java/ises/rest/entities/SimulationConfigurationTest.java
@@ -1,16 +1,63 @@
 package ises.rest.entities;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Set;
+
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+
 class SimulationConfigurationTest {
 
+	ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+	Validator validator = factory.getValidator();
+
+	@Test
+	void testValidation_shouldPass() {
+		SimulationConfiguration config = new SimulationConfiguration();
+		Set<ConstraintViolation<SimulationConfiguration>> violations = validator.validate(config);
+		assertTrue(violations.isEmpty(), "Violations should be empty");
+	}
+
+	@Test
+	void testValidation_shouldFailForNullValue() {
+		SimulationConfiguration config = new SimulationConfiguration();
+		config.setShapeMax(null);
+		Set<ConstraintViolation<SimulationConfiguration>> violations = validator.validate(config);
+		assertEquals(1, violations.size(), "Violations size mismatch");
+		violations.forEach(v -> {
+			assertEquals("shapeMax", v.getPropertyPath().toString(), "Property path mismatch");
+			assertEquals("must not be null", v.getMessage(), "Message mismatch");
+		});
+	}
+
+	@Test
+	void testValidation_shouldFailForNegativeValue() {
+		SimulationConfiguration config = new SimulationConfiguration();
+		config.setDistanceMax(-1);
+		Set<ConstraintViolation<SimulationConfiguration>> violations = validator.validate(config);
+		assertEquals(1, violations.size(), "Violations size mismatch");
+		violations.forEach(v -> {
+			assertEquals("distanceMax", v.getPropertyPath().toString(), "Property path mismatch");
+			assertEquals("must be greater than 0", v.getMessage(), "Message mismatch");
+		});
+	}
+
+	/*
+	 * This isn't really a test per se, but rather a convenient way to generate a JSON body
+	 */
 	@Disabled
 	@Test
-	void test() {
+	void generateJsonBody() {
 		ObjectMapper mapper = new ObjectMapper();
 		SimulationConfiguration config = new SimulationConfiguration();
 		String json;
@@ -18,8 +65,7 @@ class SimulationConfigurationTest {
 			json = mapper.writeValueAsString(config);
 			System.out.println(json);
 		} catch (JsonProcessingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			// ignore it
 		}
 	}
 
