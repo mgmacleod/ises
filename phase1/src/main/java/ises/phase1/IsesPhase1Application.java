c2f37670b89dfc9aaa3239acec5272905b3e4127
Matthew MacLeod <mgmacleod8@gmail.com>
2023-05-28 08:59:19 -0400

    GH-29: renamed main classes for clarity

--- phase1/src/main/java/ises/phase1/IsesPhase1Application.java
+++ phase1/src/main/java/ises/phase1/IsesPhase1Application.java
@@ -2,12 +2,9 @@ package ises.phase1;
 
 import jas.gui.ProgramFrame;
 
-// @SpringBootApplication
-public class Phase1Application {
+public class IsesPhase1Application {
 
 	public static void main(String[] args) {
-		// SpringApplication.run(Phase1Application.class, args);
-
 		new ProgramFrame();
 	}
 
--- phase2/src/main/java/ises/IsesApplicationPhase2.java
+++ phase2/src/main/java/ises/IsesApplicationPhase2.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IsesApplication {
+public class IsesApplicationPhase2 {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IsesApplication.class, args);
+		SpringApplication.run(IsesApplicationPhase2.class, args);
 	}
 
 }
