bbe940a232dac11d8344ae855902ad110068263d
Matthew MacLeod <mgmacleod8@gmail.com>
2023-06-08 20:51:03 -0400

    GH-45: added endpoints to get models by sim ID and a single model by ID

--- phase2/src/main/java/ises/rest/controllers/ModelController.java
+++ phase2/src/main/java/ises/rest/controllers/ModelController.java
@@ -1,6 +1,7 @@
 package ises.rest.controllers;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
@@ -32,6 +33,28 @@ public class ModelController {
         return new ResponseEntity<List<Long>>(models.stream().map(m -> m.getId()).collect(Collectors.toList()),
                 HttpStatus.OK);
 
+    }
+
+    @GetMapping("/simulation/{id}/model")
+    public ResponseEntity<List<ModelDto>> getAllModelsBySimulationId(@PathVariable("id") Long id) {
+        if (simulationRepository.findById(id).isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        List<ModelDto> models = modelRepository.findBySimId(id);
+
+        return new ResponseEntity<List<ModelDto>>(models, HttpStatus.OK);
+    }
+
+    @GetMapping("/model/{id}")
+    public ResponseEntity<ModelDto> getModelById(@PathVariable("id") Long id) {
+        Optional<ModelDto> modelOptional = modelRepository.findById(id);
+
+        if (modelOptional.isPresent()) {
+            return new ResponseEntity<ModelDto>(modelOptional.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<ModelDto>(HttpStatus.NOT_FOUND);
+        }
     }
 
 }
