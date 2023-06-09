87a19fd8fc87c835e025e186b62f27d43bb6a73b
Matthew MacLeod <mgmacleod8@gmail.com>
2023-06-09 10:18:50 -0400

    GH-45: added endpoints for GRNs

---
+++ phase2/src/main/java/ises/rest/controllers/GrnController.java
@@ -0,0 +1,61 @@
+package ises.rest.controllers;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import ises.rest.entities.SimulationConfiguration;
+import ises.rest.entities.dto.GrnDto;
+import ises.rest.jpa.GrnRepository;
+import ises.rest.jpa.SimulationConfigurationRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+public class GrnController {
+
+    private final SimulationConfigurationRepository simulationRepository;
+    private final GrnRepository grnRepository;
+
+    @GetMapping("/simulation/{id}/grn/ids")
+    public ResponseEntity<List<Long>> getAllGrnIdsBySimulationId(@PathVariable("id") Long id) {
+        Optional<SimulationConfiguration> configOptional = simulationRepository.findById(id);
+
+        if (configOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return new ResponseEntity<>(grnRepository.findByConfig(configOptional.get()).stream().map(g -> g.getId())
+                .collect(Collectors.toList()), HttpStatus.OK);
+
+    }
+
+    @GetMapping("/simulation/{id}/grn")
+    public ResponseEntity<List<GrnDto>> getAllGrnsBySimulationId(@PathVariable("id") Long id) {
+        Optional<SimulationConfiguration> configOptional = simulationRepository.findById(id);
+
+        if (configOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return new ResponseEntity<>(grnRepository.findByConfig(configOptional.get()), HttpStatus.OK);
+    }
+
+    @GetMapping("/grn/{id}")
+    public ResponseEntity<GrnDto> getGrnById(@PathVariable("id") Long id) {
+        Optional<GrnDto> grnOptional = grnRepository.findById(id);
+
+        if (grnOptional.isPresent()) {
+            return new ResponseEntity<GrnDto>(grnOptional.get(), HttpStatus.OK);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+}
--- phase2/src/main/java/ises/rest/jpa/GrnRepository.java
+++ phase2/src/main/java/ises/rest/jpa/GrnRepository.java
@@ -1,9 +1,14 @@
 package ises.rest.jpa;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import ises.rest.entities.SimulationConfiguration;
 import ises.rest.entities.dto.GrnDto;
 
 public interface GrnRepository extends JpaRepository<GrnDto, Long> {
+
+    public List<GrnDto> findByConfig(SimulationConfiguration config);
 
 }
