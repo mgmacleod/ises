4ceefe5b76db5f81a326fd0f666767b001a04423
Matthew MacLeod <mgmacleod8@gmail.com>
2023-06-08 20:33:08 -0400

    GH-45: added repo method to get models by sim id

--- phase2/src/main/java/ises/rest/jpa/ModelStatsRepository.java
+++ phase2/src/main/java/ises/rest/jpa/ModelStatsRepository.java
@@ -1,9 +1,13 @@
 package ises.rest.jpa;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import ises.rest.entities.dto.ModelDto;
 
 public interface ModelStatsRepository extends JpaRepository<ModelDto, Long> {
+
+    public List<ModelDto> findBySimId(Long simId);
 
 }
